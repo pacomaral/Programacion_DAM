56c49a2dbde068e314f94e310deed7d311bb5105
Paco Maravilla Aliaga <maravilleta_6@hotmail.com>
2017-02-28 19:48:47 +0100

    3

--- Proyecto_ListaPalabras/src/Principal/Principal.java
+++ Proyecto_ListaPalabras/src/Principal/Principal.java
@@ -1,5 +1,6 @@
 package Principal;
 import Visual.Ventana;
+import Visual.Ventana_v2;
 
 public class Principal {
 
@@ -7,8 +8,11 @@ public class Principal {
 	
 	public static void main(String[] args) {
 
-		Ventana ventana1=new Ventana();
-		ventana1.setVisible(true);
+		//Ventana ventana1=new Ventana();
+		//ventana1.setVisible(true);
+		
+		Ventana_v2 ventana=new Ventana_v2();
+		ventana.setVisible(true);
 
 	}
 
