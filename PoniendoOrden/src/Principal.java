dd8dc7c162c1d6b0ad64cda4127732e1371c506e
Paco Maravilla Aliaga <maravilleta_6@hotmail.com>
2017-05-08 15:40:43 +0200

    Poniendo orden

---
+++ PoniendoOrden/src/Principal.java
@@ -0,0 +1,14 @@
+import Visual.Ventana1;
+import Visual.Ventana2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Ventana1 v1 = new Ventana1();
+		v1.setVisible(true);
+		Ventana2 v2 = new Ventana2();
+		v2.setVisible(true);
+
+	}
+
+}
